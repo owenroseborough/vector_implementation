a714af4f14bac4fd23bd3c11c8596d40316a85ca
Owen Roseborough <orosebor@uwo.ca>
2024-12-02 16:54:36 -0500

    Changes Dec 02 2024

--- Vector Implementation/owens_vector.h
+++ Vector Implementation/owens_vector.h
@@ -9,6 +9,8 @@ private:
     // array to hold data
     T* m_array;
     size_t m_size;
+    T* m_begin;
+    T* m_end;
 
 public:
     owens_vector();
@@ -18,16 +20,62 @@ public:
     void operator=(std::initializer_list<T> bracedInitializer);
     size_t size();
     void print();
+    class owens_iterator {
+    private:
+        T* m_pointer;
+
+    public:
+        void owens_iterator_begin() {
+            //set value of owens_iterator pointer to beginning of array
+            m_pointer = m_begin;
+        }
+        void owens_iterator_end() {
+            //set value of owens_iterator pointer to ending of array
+            m_pointer = m_end;
+        }
+        T operator*() {
+            //return value pointed to by iterator
+            return *m_pointer;
+        }
+        void operator++() {
+            //increment owens_iterator
+            if (m_pointer < m_end) {
+                m_pointer++;
+            }
+        }
+        void operator--() {
+            //decrement owens_iterator
+            if (m_pointer > m_begin) {
+                m_pointer--;
+            }
+        }
+    };
+    owens_iterator begin() {
+        //need to return instance of owens_iterator pointing to beginning of array
+        owens_iterator iterator();
+        iterator.owens_iterator_begin();
+        return iterator;
+    }
+    owens_iterator end() {
+        //need to return instance of owens_iterator pointing to ending of array
+        owens_iterator iterator();
+        iterator.owens_iterator_end();
+        return iterator;
+    }
 };
 template <typename T> owens_vector<T>::owens_vector()
 {
     m_array = new T[100];
     m_size = 100;
+    m_begin = m_array;
+    m_end = m_array + m_size;
 }
 template <typename T> owens_vector<T>::owens_vector(size_t num)
 {
     m_array = new T[num];
     m_size = num;
+    m_begin = m_array;
+    m_end = m_array + m_size;
 }
 template <typename T> owens_vector<T>::~owens_vector<T>() {
     delete[] m_array;
@@ -47,13 +95,18 @@ template <typename T> void owens_vector<T>::operator=(std::initializer_list<T> b
         m_size = bracedInitializer.size();
         m_array = new T[m_size];
     }
+    else if (bracedInitializer.size() == 0) {
+        m_size = 0;
+        m_end = m_begin;
+        return;
+    }
     // Copy elements from the initializer list to the vector
     size_t index = 0;
     for (const T& value : bracedInitializer) {
         m_array[index] = value;
         index++;
     }
-    
+    m_end = m_begin + (--index);  //update m_end pointer to point to correct ending of array
 }
 template <typename T> size_t owens_vector<T>::size()
 {
@@ -65,5 +118,4 @@ template <typename T> void owens_vector<T>::print()
         cout << value << " ";
     }
 }
-
 #endif // OWENS_VECTOR_H
